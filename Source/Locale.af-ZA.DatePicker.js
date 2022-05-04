1b1050d0c624c680478c2bfa3276975c9e778c3a
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-05-04 03:53:07 +0300

    Update Locale.af-ZA.DatePicker.js

--- Source/Locale.af-ZA.DatePicker.js
+++ Source/Locale.af-ZA.DatePicker.js
@@ -8,7 +8,6 @@ provides: Locale.af-ZA.DatePicker
 ...
 */
 
-
 Locale.define('af-ZA', 'DatePicker', {
 	select_a_time: 'Kies \'n tyd',
 	use_mouse_wheel: 'Gebruik die muiswiel om vinnig \'n waarde te verander',
