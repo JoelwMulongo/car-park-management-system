defb865994d7fcc06a97d0fb59efe9140ab8273d
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-05-15 17:59:39 +0300

    Update head.php

--- inc/head.php
+++ inc/head.php
@@ -3,8 +3,7 @@
 <link rel="stylesheet" href="styles/pager.css" type="text/css" />
 <script src="js/jquery.js"></script>
 <script src="js/myjs.js"></script>
-
-	<script src="mootools-core.js" type="text/javascript"></script>
+        <script src="mootools-core.js" type="text/javascript"></script>
 	<script src="mootools-more.js" type="text/javascript"></script>
 	<script src="Source/Locale.en-US.DatePicker.js" type="text/javascript"></script>
 	<script src="Source/Picker.js" type="text/javascript"></script>
